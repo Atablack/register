f63efb9ab7599275a170ce599789b0b212bfaf80
Atabek Kudaikulov <atabeckk@gmail.com>
2023-04-11 21:41:54 +0600

    register field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { appendErrors, useForm } from "react-hook-form";
 
-function App() {
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="form_hook">
+      <input {...register("name")} placeholder="name"/> <br />
+      <input {...register("surname", { required: true })} placeholder="surname" /> <br />
+      <select {...register("gender")}>
+        <option value={"female"}>female</option>
+        <option value={"male"}>male</option>
+        <option value={"other"}>other</option>
+      </select> <br />
+      <select {...register('year of birth')}>
+        <option value={2023}>2023</option>
+        <option value={2022}>2022</option>
+        <option value={2021}>2021</option>
+        <option value={2020}>2020</option>
+        <option value={2019}>2019</option>
+        <option value={2018}>2018</option>
+        <option value={2017}>2017</option>
+        <option value={2016}>2016</option>
+        <option value={2015}>2015</option>
+        <option value={2014}>2014</option>
+        <option value={2013}>2013</option>
+        <option value={2012}>2012</option>
+        <option value={2011}>2011</option>
+        <option value={2010}>2010</option>
+        <option value={2009}>2009</option>
+        <option value={2008}>2008</option>
+        <option value={2007}>2007</option>
+        <option value={2006}>2006</option>
+        <option value={2005}>2005</option>
+        <option value={2004}>2004</option>
+        <option value={2003}>2003</option>
+        <option value={2002}>2002</option>
+        <option value={2001}>2001</option>
+        <option value={2000}>2000</option>
+        <option value={1999}>1999</option>
+        <option value={1998}>1998</option>
+        <option value={1997}>1997</option>
+        <option value={1996}>1996</option>
+        <option value={1995}>1995</option>
+        <option value={1994}>1994</option>
+        <option value={1993}>1993</option>
+        <option value={1992}>1992</option>
+        <option value={1991}>1991</option>
+        <option value={1990}>1990</option>
+        <option value={1989}>1989</option>
+        <option value={1988}>1988</option>
+        <option value={1987}>1987</option>
+        <option value={1986}>1986</option>
+        <option value={1985}>1985</option>
+      </select> <br />
+      <input type="submit" value={'send'} className="send_btn"/>
+    </form>
   );
 }
-
-export default App;
